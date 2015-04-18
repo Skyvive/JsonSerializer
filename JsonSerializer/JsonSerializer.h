b8f9028f29341b6035722f3661dcb72dedcb887c
bradhilton <brad.hilton.nw@gmail.com>
2015-04-18 16:11:09 -0600

    Initial Commit

---
+++ JsonSerializer/JsonSerializer.h
@@ -0,0 +1,19 @@
+//
+//  JsonSerializer.h
+//  JsonSerializer
+//
+//  Created by Bradley Hilton on 4/18/15.
+//  Copyright (c) 2015 Skyvive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JsonSerializer.
+FOUNDATION_EXPORT double JsonSerializerVersionNumber;
+
+//! Project version string for JsonSerializer.
+FOUNDATION_EXPORT const unsigned char JsonSerializerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JsonSerializer/PublicHeader.h>
+
+
