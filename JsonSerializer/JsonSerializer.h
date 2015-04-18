462d650ccb93277a9282ce2185497b7d809bbeeb
bradhilton <brad.hilton.nw@gmail.com>
2015-04-18 16:24:24 -0600

    Revert "Initial commit"
    This reverts commit 9fb0d07d423ad58964649b759e9254711398d757.

--- JsonSerializer/JsonSerializer.h
+++ JsonSerializer/JsonSerializer.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Skyvive. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 //! Project version number for JsonSerializer.
 FOUNDATION_EXPORT double JsonSerializerVersionNumber;
--- JsonSerializerTests/JsonSerializerTests.swift
+++ JsonSerializerTests/JsonSerializerTests.swift
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Skyvive. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 import XCTest
 
 class JsonSerializerTests: XCTestCase {
