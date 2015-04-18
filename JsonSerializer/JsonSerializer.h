9fb0d07d423ad58964649b759e9254711398d757
bradhilton <brad.hilton.nw@gmail.com>
2015-04-18 16:24:02 -0600

    Initial commit

--- JsonSerializer/JsonSerializer.h
+++ JsonSerializer/JsonSerializer.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Skyvive. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for JsonSerializer.
 FOUNDATION_EXPORT double JsonSerializerVersionNumber;
--- JsonSerializerTests/JsonSerializerTests.swift
+++ JsonSerializerTests/JsonSerializerTests.swift
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Skyvive. All rights reserved.
 //
 
-import UIKit
+import Cocoa
 import XCTest
 
 class JsonSerializerTests: XCTestCase {
